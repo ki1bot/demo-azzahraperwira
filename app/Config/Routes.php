e47d6a370ffde21b1a947855cae5bc5b498210b7
Rifqi Kibot <f8goodspoof@gmail.com>
2026-06-06 23:15:05 +0700

    nambah link

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -37,27 +37,31 @@ $routes->get('index.php/home/informasi', 'Home::informasi');
 
 $routes->get('admin/login/index.php', 'Admin\Otentikasi::login');
 $routes->post('admin/login/index.php', 'Admin\Otentikasi::prosesLogin');
+
 $routes->get('admin/logout/index.php', 'Admin\Otentikasi::logout');
 
+$routes->get('admin/login', 'Admin\Otentikasi::login');
+$routes->post('admin/login', 'Admin\Otentikasi::prosesLogin');
+$routes->get('admin/logout', 'Admin\Otentikasi::logout');
+
 $routes->group('admin', ['filter' => 'filteradmin'], static function (RouteCollection $routes) {
+    $routes->get('/', 'Admin\KelolaHalaman::dashboard');
+
     $routes->get('dashboard/index.php', 'Admin\KelolaHalaman::dashboard');
+    $routes->get('dashboard', 'Admin\KelolaHalaman::dashboard');
 
     $routes->get('ubah-password/index.php', 'Admin\Otentikasi::ubahPassword');
-    $routes->post('ubah-password/proses/index.php', 'Admin\Otentikasi::prosesUbahPassword');
+    $routes->post('ubah-password/index.php', 'Admin\Otentikasi::prosesUbahPassword');
+    $routes->get('ubah-password', 'Admin\Otentikasi::ubahPassword');
+    $routes->post('ubah-password', 'Admin\Otentikasi::prosesUbahPassword');
 
     $routes->get('beranda/index.php', 'Admin\KelolaHalaman::index/beranda');
-    $routes->get('beranda/tambah/index.php', 'Admin\KelolaHalaman::tambah/beranda');
-    $routes->post('beranda/simpan/index.php', 'Admin\KelolaHalaman::simpan/beranda');
     $routes->get('beranda/edit/(:num)/index.php', 'Admin\KelolaHalaman::edit/beranda/$1');
     $routes->post('beranda/update/(:num)/index.php', 'Admin\KelolaHalaman::update/beranda/$1');
-    $routes->post('beranda/hapus/(:num)/index.php', 'Admin\KelolaHalaman::hapus/beranda/$1');
 
     $routes->get('profile/index.php', 'Admin\KelolaHalaman::index/profile');
-    $routes->get('profile/tambah/index.php', 'Admin\KelolaHalaman::tambah/profile');
-    $routes->post('profile/simpan/index.php', 'Admin\KelolaHalaman::simpan/profile');
     $routes->get('profile/edit/(:num)/index.php', 'Admin\KelolaHalaman::edit/profile/$1');
     $routes->post('profile/update/(:num)/index.php', 'Admin\KelolaHalaman::update/profile/$1');
-    $routes->post('profile/hapus/(:num)/index.php', 'Admin\KelolaHalaman::hapus/profile/$1');
 
     $routes->get('tenaga-pengajar/index.php', 'Admin\KelolaHalaman::index/tenaga-pengajar');
     $routes->get('tenaga-pengajar/tambah/index.php', 'Admin\KelolaHalaman::tambah/tenaga-pengajar');
@@ -67,32 +71,20 @@ $routes->group('admin', ['filter' => 'filteradmin'], static function (RouteColle
     $routes->post('tenaga-pengajar/hapus/(:num)/index.php', 'Admin\KelolaHalaman::hapus/tenaga-pengajar/$1');
 
     $routes->get('unit-kb-tk/index.php', 'Admin\KelolaHalaman::index/unit-kb-tk');
-    $routes->get('unit-kb-tk/tambah/index.php', 'Admin\KelolaHalaman::tambah/unit-kb-tk');
-    $routes->post('unit-kb-tk/simpan/index.php', 'Admin\KelolaHalaman::simpan/unit-kb-tk');
     $routes->get('unit-kb-tk/edit/(:num)/index.php', 'Admin\KelolaHalaman::edit/unit-kb-tk/$1');
     $routes->post('unit-kb-tk/update/(:num)/index.php', 'Admin\KelolaHalaman::update/unit-kb-tk/$1');
-    $routes->post('unit-kb-tk/hapus/(:num)/index.php', 'Admin\KelolaHalaman::hapus/unit-kb-tk/$1');
 
     $routes->get('unit-tpq/index.php', 'Admin\KelolaHalaman::index/unit-tpq');
-    $routes->get('unit-tpq/tambah/index.php', 'Admin\KelolaHalaman::tambah/unit-tpq');
-    $routes->post('unit-tpq/simpan/index.php', 'Admin\KelolaHalaman::simpan/unit-tpq');
     $routes->get('unit-tpq/edit/(:num)/index.php', 'Admin\KelolaHalaman::edit/unit-tpq/$1');
     $routes->post('unit-tpq/update/(:num)/index.php', 'Admin\KelolaHalaman::update/unit-tpq/$1');
-    $routes->post('unit-tpq/hapus/(:num)/index.php', 'Admin\KelolaHalaman::hapus/unit-tpq/$1');
 
     $routes->get('unit-dc/index.php', 'Admin\KelolaHalaman::index/unit-dc');
-    $routes->get('unit-dc/tambah/index.php', 'Admin\KelolaHalaman::tambah/unit-dc');
-    $routes->post('unit-dc/simpan/index.php', 'Admin\KelolaHalaman::simpan/unit-dc');
     $routes->get('unit-dc/edit/(:num)/index.php', 'Admin\KelolaHalaman::edit/unit-dc/$1');
     $routes->post('unit-dc/update/(:num)/index.php', 'Admin\KelolaHalaman::update/unit-dc/$1');
-    $routes->post('unit-dc/hapus/(:num)/index.php', 'Admin\KelolaHalaman::hapus/unit-dc/$1');
 
     $routes->get('unit-lansia/index.php', 'Admin\KelolaHalaman::index/unit-lansia');
-    $routes->get('unit-lansia/tambah/index.php', 'Admin\KelolaHalaman::tambah/unit-lansia');
-    $routes->post('unit-lansia/simpan/index.php', 'Admin\KelolaHalaman::simpan/unit-lansia');
     $routes->get('unit-lansia/edit/(:num)/index.php', 'Admin\KelolaHalaman::edit/unit-lansia/$1');
     $routes->post('unit-lansia/update/(:num)/index.php', 'Admin\KelolaHalaman::update/unit-lansia/$1');
-    $routes->post('unit-lansia/hapus/(:num)/index.php', 'Admin\KelolaHalaman::hapus/unit-lansia/$1');
 
     $routes->get('informasi/index.php', 'Admin\KelolaHalaman::index/informasi');
     $routes->get('informasi/tambah/index.php', 'Admin\KelolaHalaman::tambah/informasi');
@@ -102,14 +94,8 @@ $routes->group('admin', ['filter' => 'filteradmin'], static function (RouteColle
     $routes->post('informasi/hapus/(:num)/index.php', 'Admin\KelolaHalaman::hapus/informasi/$1');
 
     $routes->get('footer/index.php', 'Admin\KelolaHalaman::index/footer');
-    $routes->get('footer/tambah/index.php', 'Admin\KelolaHalaman::tambah/footer');
-    $routes->post('footer/simpan/index.php', 'Admin\KelolaHalaman::simpan/footer');
     $routes->get('footer/edit/(:num)/index.php', 'Admin\KelolaHalaman::edit/footer/$1');
     $routes->post('footer/update/(:num)/index.php', 'Admin\KelolaHalaman::update/footer/$1');
-    $routes->post('footer/hapus/(:num)/index.php', 'Admin\KelolaHalaman::hapus/footer/$1');
-
-    $routes->get('/', 'Admin\KelolaHalaman::dashboard');
-    $routes->get('dashboard', 'Admin\KelolaHalaman::dashboard');
 
     $routes->get('halaman/(:segment)', 'Admin\KelolaHalaman::index/$1');
     $routes->get('halaman/(:segment)/tambah', 'Admin\KelolaHalaman::tambah/$1');
